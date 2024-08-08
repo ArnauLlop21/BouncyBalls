f30626571f80befccdda91e9112f6d8e44b425a6
Arnau Llop Iglesias <arnau.llop2001@gmail.com>
2024-08-08 19:16:11 +0200

    #2 Second ball added
    Second ball added to perform collision detection and response

--- Python/ball.py
+++ Python/ball.py
@@ -1,7 +1,8 @@
 class Ball:
-    def __init__(self, radius, circle_x, circle_y, speed_x, speed_y):
+    def __init__(self, radius, circle_x, circle_y, speed_x, speed_y, color):
         self.radius = radius
         self.circle_x = circle_x
         self.circle_y = circle_y
         self.speed_x = speed_x
         self.speed_y = speed_y
+        self.color = color
--- Python/main.py
+++ Python/main.py
@@ -2,74 +2,85 @@ import pygame
 import time
 import ball
  
+# DEFINITION OF CRITICAL OBJECTS
 SCREEN = pygame.display.set_mode ((600, 400))
 MOUSE = pygame.mouse
+BALLS = []
 
-gravity = 0.1
+# DEFINITION OF CONFIG VARIABLES
 coefficient_of_restitution_y = 0.7
 coefficient_of_restitution_x = 0.5
 coefficient_of_friction = 0.1
+gravity = 0.1
 
-b1 = ball.Ball(30,50,50,10,0)
-
+# DEFINITION OF AUXILIAR VARIABLES
 alive = True
 
+# INTANTIATION OF GAME COMPONENTS
+b1 = ball.Ball(30,50,50,10,0,(255,0,0))
+b2 = ball.Ball(20,40,35,5,5,(255,240,156))
+BALLS.append(b1)
+BALLS.append(b2)
 clock = pygame.time.Clock()
+
+
 while True :
     for event in pygame.event.get () :
         if event.type == pygame.QUIT :
             quit ()
     click_state = MOUSE.get_pressed(num_buttons=3)
-    if any(click_state) == True:
-        b1.speed_x += 1 if (b1.speed_x >= 0) else -1
-        b1.speed_y += -1
-        alive = True
-    if alive == True: 
-        SCREEN.fill ((0, 0, 0)) # Background color to rgb 0,0,0 (Black)
+    for curr_ball in BALLS:
+        if any(click_state) == True:
+            curr_ball.speed_x += 1 if (curr_ball.speed_x >= 0) else -1
+            curr_ball.speed_y += -1
+            alive = True
+        if alive == True: 
+            SCREEN.fill ((0, 0, 0)) # Background color to rgb 0,0,0 (Black)
 
-        pygame.draw.circle (SCREEN, (255, 0, 0), (b1.circle_x,b1.circle_y), radius=b1.radius)
+            for i in BALLS:
+                pygame.draw.circle (SCREEN, i.color, (i.circle_x,i.circle_y), radius=i.radius)
 
-        b1.speed_y += gravity
-        b1.circle_y += b1.speed_y
-        b1.circle_x += b1.speed_x
+            curr_ball.speed_y += gravity
+            curr_ball.circle_y += curr_ball.speed_y
+            curr_ball.circle_x += curr_ball.speed_x
+
+            if -0.07 <= curr_ball.speed_y <= 0.07:
+                curr_ball.speed_y = 0
+                if curr_ball.speed_x > 0:
+                    curr_ball.speed_x -= coefficient_of_friction
+                else:
+                    curr_ball.speed_x += coefficient_of_friction
+
+            if -0.07 <= curr_ball.speed_x <= 0.07:
+                curr_ball.speed_x = 0
+
+            pygame.display.update ()
+    
+            if (curr_ball.speed_y == 0) and (curr_ball.speed_x == 0):
+                alive = False
+
+            if curr_ball.circle_y + curr_ball.radius >= SCREEN.get_height():
+                curr_ball.circle_y = SCREEN.get_height() - curr_ball.radius
+                curr_ball.speed_y *= -coefficient_of_restitution_y  # reverse direction with some energy loss
+            elif curr_ball.circle_y - curr_ball.radius <= 0:
+                curr_ball.circle_y = curr_ball.radius
+                curr_ball.speed_y *= -coefficient_of_restitution_y  # reverse direction with some energy loss
+                # NEGATIVE speed_y means it goes up
+                # POSITIVE speed_y means it goes down
+                # :)
+            if curr_ball.circle_x + curr_ball.radius > SCREEN.get_width():
+                curr_ball.circle_x = SCREEN.get_width() - curr_ball.radius
+                curr_ball.speed_x *= -coefficient_of_restitution_x
+            elif curr_ball.circle_x - curr_ball.radius <= 0:
+                curr_ball.circle_x = curr_ball.radius
+                curr_ball.speed_x *= -coefficient_of_restitution_x
+
+            clock.tick()
+            print(clock.get_fps())
         
-        if -0.07 <= b1.speed_y <= 0.07:
-            b1.speed_y = 0
-            if b1.speed_x > 0:
-                b1.speed_x -= coefficient_of_friction
-            else:
-                b1.speed_x += coefficient_of_friction
-
-        if -0.07 <= b1.speed_x <= 0.07:
-            b1.speed_x = 0
-
-        pygame.display.update ()
-        time.sleep(0.01)
-
-        if (b1.speed_y == 0) and (b1.speed_x == 0):
-            alive = False
-
-        if b1.circle_y + b1.radius >= SCREEN.get_height():
-            b1.circle_y = SCREEN.get_height() - b1.radius
-            b1.speed_y *= -coefficient_of_restitution_y  # reverse direction with some energy loss
-        elif b1.circle_y - b1.radius <= 0:
-            b1.circle_y = b1.radius
-            b1.speed_y *= -coefficient_of_restitution_y  # reverse direction with some energy loss
-            # NEGATIVE speed_y means it goes up
-            # POSITIVE speed_y means it goes down
-            # :)
-        if b1.circle_x + b1.radius > SCREEN.get_width():
-            b1.circle_x = SCREEN.get_width() - b1.radius
-            b1.speed_x *= -coefficient_of_restitution_x
-        elif b1.circle_x - b1.radius <= 0:
-            b1.circle_x = b1.radius
-            b1.speed_x *= -coefficient_of_restitution_x
         
-        clock.tick()
-        print(clock.get_fps())
-        
-    else:
-        time.sleep(0.01)
+    
+    time.sleep(0.01)
 
 
 
