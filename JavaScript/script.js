807ef48380053020bb95e4c50d82763df8b6c9e7
Arnau Llop Iglesias <arnau.llop2001@gmail.com>
2024-08-17 14:17:10 +0200

    #14 Ball is already bouncing

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,25 +2,56 @@ var canvas = document.getElementById("myCanvas");
 
 // Check if canva exists
 if (canvas) {
-    // Obtain the 2d container to draw
+    // Get the 2D drawing context
     var ctx = canvas.getContext("2d");
 
-    // Define the circle coordinates
+    // Define the center coordinates of the circle
     var centerX = canvas.width / 2;
     var centerY = canvas.height / 2;
+    var speedX = 6;
+    var speedY = 2;
+    const coefficientOfRestitution = 1
 
-    // Define the circle radius
+    // Define the radius of the circle
     var radius = 50;
 
-    // Begin a new path to draw
-    ctx.beginPath();
+    // Function to update and draw the content of the canvas
+    function update() {
+        // Clear the canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Draw the circle
-    ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+        // Update the circle's coordinates (e.g., moving it in a circular path)
+        centerX += speedX;
+        centerY += speedY;
+        
+        if (centerX + radius >= canvas.width){
+            centerX = canvas.width - radius;
+            speedX *= -coefficientOfRestitution;
+        }else if(centerX - radius <= 0){
+            centerX = radius;
+            speedX *= -coefficientOfRestitution;
+        }
 
-    // Fill the circle with a certain color.
-    ctx.fillStyle = "blue";
-    ctx.fill();
+        if (centerY + radius >= canvas.height){
+            centerY = canvas.height - radius;
+            speedY *= -coefficientOfRestitution;
+        }else if(centerY - radius <= 0){
+            centerY = radius;
+            speedY *= -coefficientOfRestitution;
+        }
+        // Draw the circle
+        ctx.beginPath();
+        ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+        ctx.fillStyle = "purple";
+        ctx.fill();
+
+
+        // Request the browser to call the update function on the next frame
+        requestAnimationFrame(update);
+    }
+
+    // Start the animation
+    update();
 } else {
-    console.error("El canvas no s'ha trobat.");
+    console.error("Canvas not found.");
 }
